407e48ec362d0aa6c2f664b7e3cbcf3d97759e9a
zaurmag411@gmail.com <guKGyVmC7o>
2023-11-13 18:14:54 +0300

    Feat: add result screen

---
+++ components/test/test-result.vue
@@ -0,0 +1,73 @@
+<template>
+<section class="test__result">
+  <div class="test__result-content">
+    <header class="test__result-header">
+      <nuxt-img class="test__result-header-img" src="/images/test/start-screen-image.webp" width="80" />
+
+      <h2 class="test__result-title">Результат теста - заголовок</h2>
+    </header>
+
+    <div class="test__result-text">
+      <p>
+        <strong>
+          Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit.
+        </strong>
+      </p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dicta
+        doloribus error officiis quam qui!</p>
+    </div>
+
+    <button
+      class="btn test__result-btn"
+      @click="$emit('restart')"
+    >
+      Пройти еще раз
+    </button>
+  </div>
+
+  <div class="test__result-image">
+    <nuxt-img src="/images/test/start-screen-image.webp" width="400" />
+  </div>
+</section>
+</template>
+
+<script setup lang="ts">
+defineEmits(['restart'])
+</script>
+
+<style scoped lang="sass">
+.test__result
+  display: grid
+  grid-gap: 2rem
+  grid-template-columns: 50% 50%
+
+  &-content
+    display: grid
+    grid-gap: 1.5rem
+    align-content: start
+    justify-items: start
+
+  &-text
+    display: grid
+    grid-gap: 0.5rem
+
+  &-header
+    display: grid
+    grid-gap: 1rem
+    grid-auto-flow: column
+    grid-template-columns: auto 1fr
+    align-items: start
+
+  &-title
+    font-size: 1.8rem
+
+  &-btn
+    background-color: $blue
+    color: $white
+    padding: 0.5rem 1rem
+
+    &:hover
+      background-color: darken($blue, 10%)
+</style>
--- components/test/test.vue
+++ components/test/test.vue
@@ -2,16 +2,20 @@
   <div class="test">
    <test-start v-if="isStart" @start="isStart = false" />
 
-    <template
-      v-for="question in questions"
-      :key="question.id"
-    >
-      <test-question
-        v-if="activeQuestionNum === question.num && !isStart"
-        :question="question"
-        :total-questions="questions.length"
-        @next="goToNextQuestion"
-      />
+    <template v-else>
+      <template
+        v-for="question in questions"
+        :key="question.id"
+      >
+        <test-question
+          v-if="activeQuestionNum === question.num && !isStart && !isResult"
+          :question="question"
+          :total-questions="questions.length"
+          @next="goToNextQuestion"
+        />
+      </template>
+
+      <test-result v-if="isResult" @restart="restartTest" />
     </template>
   </div>
 </template>
@@ -30,6 +34,11 @@ const goToNextQuestion = (isShowResult: boolean) => {
   } else {
     isResult.value = isShowResult
   }
+}
+
+const restartTest = () => {
+  isStart.value = true
+  isResult.value = false
 }
 </script>
 
